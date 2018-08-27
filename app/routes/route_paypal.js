defcfe93df334ba1253ed1de8c5a9ca1bcb6df36
frshafi49 <frshafi49@gmail.com>
2018-08-27 23:51:28 +0600

    added comment in route_paypal.js file

--- app/routes/route_paypal.js
+++ app/routes/route_paypal.js
@@ -17,8 +17,15 @@ paypal.configure({
     // Paypal Pyment Section ===============
     // =====================================
 
+
+    // this end point will be called when encourage btton is pressed
     app.post('/pay',(req,res)=>{
 
+
+      // this json object will be sent to next create function of paypal
+      // for now we are giving it as a demo,but in real payment, prices and
+      // items will be updated from form and will be received by body parser and
+      // pass it to this json
       const create_payment_json = {
           "intent": "sale",
           "payer": {
@@ -46,13 +53,17 @@ paypal.configure({
           }]
       };
 
-
+     // paypal will create a payment template using this function based on our 'create_payment_json'
      paypal.payment.create(create_payment_json, function (error, payment) {
           if (error) {
               throw error;
           } else {
               console.log("Create Payment Response");
               console.log(payment);
+
+              // if it gets everything ok it will give us some link in an array
+              // we have to redirect to 'approval_url' link
+              // So, we are running a for loop to get extract this desired url
               for(let i=0;i<payment.links.length;i++){
                   if(payment.links[i].rel==='approval_url'){
                       res.redirect(payment.links[i].href);
@@ -62,6 +73,8 @@ paypal.configure({
       });
     });
 
+    // if our payment is done successfully, this route will be called
+    // because it is mentioned in 'redirect_urls' object of 'create_payment_json'
     app.get('/success',(req,res)=>{
       const payerId = req.query.PayerID;
       const paymentId = req.query.paymentId;
@@ -77,6 +90,9 @@ paypal.configure({
           }]
       };
 
+      // this method will be called by paypal when our payment is done
+      // from the response of this function, we will get a lots of info
+      // about the payment, which we can use in our website
       paypal.payment.execute(paymentId,execute_payment_json,function(error,payment){
 
           if(error){
@@ -91,6 +107,8 @@ paypal.configure({
       });
   });
 
+  // if user canceled the payment in the middle of the payment process,
+  // this rout will be called
   app.get('/cancel',(req,res)=>{
   
       res.render('paypal_response/paypal_cancel.ejs');
